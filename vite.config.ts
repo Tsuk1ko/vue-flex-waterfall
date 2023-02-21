2dda5c7fcf12f98e91947e0478cdb392ba12770d
神代綺凛 <i@loli.best>
2023-02-21 15:07:40 +0800

    feat: vite@4 and dts plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,39 +2,41 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import dts from 'vite-plugin-dts';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 const isDemo = process.env.CONFIG_TYPE === 'demo';
 
-const demoConfig = defineConfig({
-  base: '',
-  plugins: [vue(), cssInjectedByJsPlugin()],
-  build: {
-    target: 'es2015',
-    outDir: 'demo',
-    sourcemap: true,
-  },
-});
-
-const libConfig = defineConfig({
-  plugins: [vue()],
-  build: {
-    target: 'es2015',
-    sourcemap: true,
-    // cssCodeSplit: false,
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'VueFlexWaterfall',
+const demoConfig = () =>
+  defineConfig({
+    base: '',
+    plugins: [vue(), cssInjectedByJsPlugin()],
+    build: {
+      target: 'es2015',
+      outDir: 'demo',
+      sourcemap: true,
     },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue',
+  });
+
+const libConfig = () =>
+  defineConfig({
+    plugins: [vue(), dts({ copyDtsFiles: false })],
+    build: {
+      target: 'es2015',
+      sourcemap: true,
+      lib: {
+        entry: resolve(__dirname, 'src/index.ts'),
+        name: 'VueFlexWaterfall',
+      },
+      rollupOptions: {
+        external: ['vue'],
+        output: {
+          globals: {
+            vue: 'Vue',
+          },
         },
       },
     },
-  },
-});
+  });
 
-export default isDemo ? demoConfig : libConfig;
+export default isDemo ? demoConfig() : libConfig();
